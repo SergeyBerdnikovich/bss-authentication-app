1e8953b447c1a219db8701f698f307fc01e275ba
Sergey Berdnikovich <sergey.berdnikovich@altoros.com>
2012-09-28 12:36:16 +0300

    role model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120928093235) do
+ActiveRecord::Schema.define(:version => 20120928093529) do
+
+  create_table "roles", :force => true do |t|
+    t.string   "role"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "roles", ["user_id"], :name => "index_roles_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "login"
